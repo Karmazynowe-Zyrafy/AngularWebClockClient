83a69af5395f03d6b234e86d03d8386c1ae2b2a1
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-11 14:14:49 +0200

    int commit for angular client transferred from WebClock API repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit, DoCheck} from '@angular/core';
+import {DataService} from './data.service';
+import {Clock} from './clock.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements  /*OnInit,*/ DoCheck {
+  clock$: Clock;
+  constructor( private  dataService: DataService) {}
+
+  ngDoCheck() {
+
+    console.log('test1' + this.dataService.getClock()) ;
+
+    return this.dataService.getClock().subscribe(data => this.clock$ = data);
+  }
+ /* ngOnInit() {
+
+    console.log('test1' + this.dataService.getClock()) ;
+
+    return this.dataService.getClock().subscribe(data => this.clock$ = data);
+  }*/
+
+}
