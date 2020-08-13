ebf8f3c402908017c27c1937e7cd3972972bcf06
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-13 11:38:01 +0200

    Navbar is now displayed by ngFor

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
     logo;
+    navbarArry;
 
     constructor() {
         this.logo = 'assets/imags/rekordlogo.png';
+        this.navbarArry = [
+            {
+                link: '#',
+                icon: 'home',
+                text: 'Strona Główna',
+            },
+            {
+                link: '#',
+                icon: 'calendar_today',
+                text: 'Historia',
+            },
+            {
+                link: '#',
+                icon: 'contact_support',
+                text: 'Informacje',
+            },
+            {
+                link: '#',
+                icon: 'power_settings_new',
+                text: 'Wyloguj',
+            },
+        ];
     }
 
     ngOnInit(): void {
