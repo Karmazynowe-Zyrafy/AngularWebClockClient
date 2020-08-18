a1bdd0a75e1e93c3c68ccded80c8b8a096d5f92e
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-18 15:15:02 +0200

    Inject service into dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../Services/api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(private apiService: ApiService) {
+  }
+}
