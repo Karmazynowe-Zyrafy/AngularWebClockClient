8011cd14d1b5e896687b99d9da2b16101f6b5beb
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-24 17:18:18 +0200

    Adding declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ClockInOutHistoryComponent } from './clock-in-out-history/clock-in-out-history.component';
+import { InOutTransformPipe } from './in-out-transform.pipe';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, DashboardComponent],
+  declarations: [AppComponent, NavbarComponent, DashboardComponent, ClockInOutHistoryComponent, InOutTransformPipe],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
