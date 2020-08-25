6e8e3ed7b10a027e22678fd4a833841f32e25ee9
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-25 11:00:54 +0200

    Hotfix

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { BalanceDto } from '../balanceDto';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {BalanceDto} from '../balanceDto';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +9,10 @@ import { BalanceDto } from '../balanceDto';
 export class ApiService {
   id = 6;
   baseUrl = 'https://localhost:44385/api';
-  clockInUrl = '${this.baseUrl}/ClockInOut/ClockIn/';
-  clockOutUrl = '${this.baseUrl}/ClockInOut/ClockOut/';
-  balanceUrl = '${this.baseUrl}/ClockInOut/Balance/';
-  BalanceToThisDayUrl = '${this.baseUrl}/ClockInOut/BalanceToThisDay/';
+  clockInUrl = `${this.baseUrl}/ClockInOut/ClockIn/`;
+  clockOutUrl = `${this.baseUrl}/ClockInOut/ClockOut/`;
+  balanceUrl = `${this.baseUrl}/ClockInOut/Balance/`;
+  BalanceToThisDayUrl = `${this.baseUrl}/ClockInOut/BalanceToThisDay/`;
   constructor(private http: HttpClient) {}
 
   public postClockIn(): Observable<HttpResponse<Response>> {
