d053f25c588c77b18f72f8cf09056e06b7a5b4a1
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-26 12:31:27 +0200

    Merge

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {BalanceDto} from '../balanceDto';
-import {ClockInOutDto} from '../clockInOutDto';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BalanceDto } from '../balanceDto';
+import { ClockInOutDto } from '../clockInOutDto';
 
 @Injectable({
   providedIn: 'root',
