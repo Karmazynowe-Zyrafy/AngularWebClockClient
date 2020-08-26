b8b8d0f36d8794174a58d66cf14b4664d2787281
Józef Salik <jozek243@gmail.com>
2020-08-26 15:18:25 +0200

    Add routerLink in navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ClockInOutHistoryComponent } from './clock-in-out-history/clock-in-out-history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'history', component: ClockInOutHistoryComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
