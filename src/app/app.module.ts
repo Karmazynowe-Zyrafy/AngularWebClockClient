41a89a4b420936a125d594f263f5d3d3c745d481
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-27 17:20:31 +0200

    Adding new favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { NavbarComponent } from './navbar/navbar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ClockInOutHistoryComponent } from './clock-in-out-history/clock-in-out-history.component';
-import { InOutTransformPipe } from './in-out-transform.pipe';
-import { DonutChartComponent } from './donut-chart/donut-chart.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MakePositivePipe } from './make-positive.pipe';
+import {HttpClientModule} from '@angular/common/http';
+import {NavbarComponent} from './navbar/navbar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ClockInOutHistoryComponent} from './clock-in-out-history/clock-in-out-history.component';
+import {InOutTransformPipe} from './in-out-transform.pipe';
+import {DonutChartComponent} from './donut-chart/donut-chart.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {MakePositivePipe} from './make-positive.pipe';
 
 import localePl from '@angular/common/locales/pl';
-import { registerLocaleData } from '@angular/common';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {registerLocaleData} from '@angular/common';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 registerLocaleData(localePl);
 
 @NgModule({
-
   declarations: [
     AppComponent,
     NavbarComponent,
@@ -32,7 +31,14 @@ registerLocaleData(localePl);
     PageNotFoundComponent,
     MakePositivePipe,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
   providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }],
 
   bootstrap: [AppComponent],
